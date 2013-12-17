b482652ec947557a7d20d563189106a1a6491419
Eitan Adler <lists@eitanadler.com>
2013-12-17 14:34:03 -0500

    makefile: remove chatty messages
    make(1) is already chatty for you.  You can even make it more verbose.
    Adding manual echo lines is pointless.

--- Build-gcc/subdir.mk
+++ Build-gcc/subdir.mk
@@ -38,10 +38,6 @@ CPP_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 %.o: ../%.cpp
-	@echo 'Building file: $<'
-	@echo 'Invoking: Cross G++ Compiler'
 	${CXX} -fopenmp -g -L/opt/local/lib -I/opt/local/include -g -Wall -fpic -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
-	@echo 'Finished building: $<'
-	@echo ' '
 
 
